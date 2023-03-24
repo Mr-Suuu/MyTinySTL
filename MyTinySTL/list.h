d0c8553944266ec75a263b68470211e1279e1bd5
sjm <742019617@qq.com>
2023-03-24 20:56:00 +0800

    2023-03-24 list

---
+++ MyTinySTL/list.h
@@ -0,0 +1,43 @@
+#ifndef MYTINYSTL_LIST_H_
+#define MYTINYSTL_LIST_H_
+
+// 这个头文件包含了一个模板类 list
+// list : 双向链表
+
+// notes:
+//
+// 异常保证：
+// mystl::list<T> 满足基本异常保证，部分函数无异常保证，并对以下等函数做强异常安全保证：
+//   * emplace_front
+//   * emplace_back
+//   * emplace
+//   * push_front
+//   * push_back
+//   * insert
+
+#include <initializer_list>
+
+#include "iterator.h"
+#include "memory.h"
+#include "functional.h"
+#include "util.h"
+#include "exceptdef.h"
+
+namespace mystl
+{
+
+    template <class T> struct list_node_base;
+    template <class T> struct list_node;
+
+    template <class T>
+    struct node_traits
+    {
+        typedef list_node_base<T>* base_ptr;
+        typedef list_node<T>*      node_ptr;
+    };
+
+
+
+} // namespace mystl
+#endif // !MYTINYSTL_LIST_H_
+
