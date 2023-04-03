4ddffa46aefdfdfd5c6d39cb3c24d10cff386e1a
sjm <742019617@qq.com>
2023-04-03 14:50:55 +0800

    2023-04-03 rb_tree：搞懂了rb_tree_insert_rebalance，rb_tree_erase_rebalance的函数逻辑（没完全懂）、rb_tree部分定义

--- MyTinySTL/rb_tree.h
+++ MyTinySTL/rb_tree.h
@@ -492,7 +492,7 @@ namespace mystl {
 //          http://blog.csdn.net/v_JULY_v/article/details/6109153
 // 参数一为新增节点，参数二为根节点
     template<class NodePtr>
-    void rb_tree_insert_rebalance(NodePtr x, NodePtr& root) noexcept {
+    void rb_tree_insert_rebalance(NodePtr x, NodePtr &root) noexcept {
         rb_tree_set_red(x);  // 新增节点都为红色
         // 若 新增节点不等于根节点 且 当前节点的父节点为红色（要进行平衡的前提是已经插入）
         while (x != root && rb_tree_is_red(x->parent)) {
@@ -501,20 +501,33 @@ namespace mystl {
                 auto uncle = x->parent->parent->right;
                 if (uncle != nullptr && rb_tree_is_red(uncle)) {
                     // case3：父节点和叔叔节点都为红
+                    // case 3: 父节点和叔叔节点都为红，令父节点和叔叔节点为黑，祖父节点为红，
+                    //         然后令祖父节点为当前节点，继续处理
+                    // 先将父节点和叔叔节点置为黑色
                     rb_tree_set_black(x->parent);
                     rb_tree_set_black(uncle);
+                    // 令祖父节点为当前节点，继续处理，向上迭代
                     x = x->parent->parent;
                     rb_tree_set_red(x);
                 } else {
                     // 无叔叔节点或叔叔节点为黑色
                     if (!rb_tree_is_lchild(x)) {
                         // case4：当前节点x为右子节点
+                        // case 4: 父节点为红，叔叔节点为 NIL 或黑色，父节点为左（右）孩子，当前节点为右（左）孩子，
+                        //         让父节点成为当前节点，再以当前节点为支点左（右）旋
+                        // 令当前指向父节点
                         x = x->parent;
+                        // 左旋，x 为当前接待你，root为根节点
                         rb_tree_rotate_left(x, root);
                     }
                     // 都转成case5：当前节点为左子节点
+                    // case 5: 父节点为红，叔叔节点为 NIL 或黑色，父节点为左（右）孩子，当前节点为左（右）孩子，
+                    //         让父节点变为黑色，祖父节点变为红色，以祖父节点为支点右（左）旋
+                    // 将父节点置为黑色
                     rb_tree_set_black(x->parent);
+                    // 将祖父节点置为红色
                     rb_tree_set_red(x->parent->parent);
+                    // 基于祖父节点右旋
                     rb_tree_rotate_right(x->parent->parent, root);
                     break;
                 }
@@ -546,6 +559,259 @@ namespace mystl {
         rb_tree_set_black(root);  // 根节点永远为黑色
     }
 
+// 删除节点后使 rb tree 重新平衡，参数一为要删除的节点，参数二为根节点，参数三为最小节点，参数四为最大节点
+//
+// 参考博客: http://blog.csdn.net/v_JULY_v/article/details/6105630
+//          http://blog.csdn.net/v_JULY_v/article/details/6109153
+    template<class NodePtr>
+    NodePtr rb_tree_erase_rebalance(NodePtr z, NodePtr &root, NodePtr &leftmost, NodePtr &rightmost) {
+        // y 是可能的替代节点，指向最终要删除的节点
+        // 若要删除的节点z的左节点或右节点为空，则返回待删除节点，否则返回待删除节点的下一个节点
+        auto y = (z->left == nullptr || z->right == nullptr) ? z : rb_tree_next(z);
+        // x 是 y 的一个独子节点或 NIL 节点
+        // 优先取左节点（若非空）
+        auto x = y->left != nullptr ? y->left : y->right;
+        // xp 为 x 的父节点
+        NodePtr xp = nullptr;
+
+        // y != z 说明 z 有两个非空子节点，此时 y 指向 z 右子树的最左节点，x 指向 y 的右子节点（因为最左，所以肯定没有左子节点，返回右子节点）
+        // 用 y 顶替 z 的位置，用 x 顶替 y 的位置，最后用 y 指向 z
+        if (y != z) {
+            // 用 y 顶替 z
+            z->left->parent = y;
+            y->left = z->left;
+
+            // 如果 y 不是 z 的右子节点，那么 z 的右子节点一定有左孩子
+            if (y != z->right) {
+                // x 替代 y 的位置（可画图方便理解）
+                xp = y->parent;
+                if (x != nullptr) {
+                    x->parent = y->parent;
+                }
+                y->parent->left = x;
+                y->right = z->right;
+                z->right->parent = y;
+            } else {
+                xp = y;
+            }
+
+            // 连接 y 与 z 的父节点
+            if (root == z) {
+                root = y;
+            } else if (rb_tree_is_lchild(z)) {
+                z->parent->left = y;
+            } else {
+                z->parent->right = y;
+            }
+            y->parent = z->parent;
+            mystl::swap(y->color, z->color);
+            y = z;
+        } else {
+            // y == z 说明 z 至多只有一个孩子
+            xp = y->parent;
+            if (x) {
+                x->parent = y->parent;
+            }
+
+            // 连接 x 与 z 的父节点
+            if (root == z) {
+                root = x;
+            } else if (rb_tree_is_lchild(z)) {
+                z->parent->left = x;
+            } else {
+                z->parent->right = x;
+            }
+
+            // 此时 z 有可能是最左节点或最右节点，更新数据
+            if (leftmost == z) {
+                leftmost = x == nullptr ? xp : rb_tree_min(x);
+            }
+            if (rightmost == z) {
+                rightmost = x == nullptr ? xp : rb_tree_max(x);
+            }
+        }
+
+        // 此时，y 指向要删除的节点，x 为替代节点，从 x 节点开始调整。
+        // 如果删除的节点为红色，树的性质没有被破坏，否则按照以下情况调整（x 为左子节点为例）：
+        // case 1: 兄弟节点为红色，令父节点为红，兄弟节点为黑，进行左（右）旋，继续处理
+        // case 2: 兄弟节点为黑色，且两个子节点都为黑色或 NIL，令兄弟节点为红，父节点成为当前节点，继续处理
+        // case 3: 兄弟节点为黑色，左子节点为红色或 NIL，右子节点为黑色或 NIL，
+        //         令兄弟节点为红，兄弟节点的左子节点为黑，以兄弟节点为支点右（左）旋，继续处理
+        // case 4: 兄弟节点为黑色，右子节点为红色，令兄弟节点为父节点的颜色，父节点为黑色，兄弟节点的右子节点
+        //         为黑色，以父节点为支点左（右）旋，树的性质调整完成，算法结束
+        if (!rb_tree_red(y)) {
+            // x 为黑色时，调整，否则直接将 x 变为黑色即可
+            while (x != root && (x == nullptr || !rb_tree_is_red(x))) {
+                if (x == xp->left) {
+                    // 如果 x 为 左子节点
+                    auto brother = xp->right;
+                    if (rb_tree_is_red(brother)) {
+                        // case 1
+                        rb_tree_set_black(brother);
+                        rb_tree_set_red(xp);
+                        rb_tree_rotate_left(xp, root);
+                        brother = xp->right;
+                    }
+                    // case 1 转为了 case 2、3、4 中的一种
+                    if ((brother->left == nullptr || !rb_tree_is_red(brother->left)) &&
+                        (brother->right == nullptr || !rb_tree_is_red(brother->right))) {
+                        // case 2
+                        rb_tree_set_red(brother);
+                        x = xp;
+                        xp = xp->parent;
+                    } else {
+                        if (brother->right == nullptr || !rb_tree_is_red(brother->right)) {
+                            // case 3
+                            if (brother->left != nullptr) {
+                                rb_tree_set_black(brother->left);
+                            }
+                            rb_tree_set_red(brother);
+                            rb_tree_rotate_right(brother, root);
+                            brother = xp->right;
+                        }
+                        // 转为 case 4
+                        brother->color = xp->color;
+                        rb_tree_set_black(xp);
+                        if (brother->right != nullptr) {
+                            rb_tree_set_black(brother->right);
+                        }
+                        rb_tree_rotate_left(xp, root);
+                        break;
+                    }
+                } else {
+                    // x 为右子节点，对称处理
+                    auto brother = xp->left;
+                    if (rb_tree_is_red(brother)) { // case 1
+                        rb_tree_set_black(brother);
+                        rb_tree_set_red(xp);
+                        rb_tree_rotate_right(xp, root);
+                        brother = xp->left;
+                    }
+                    if ((brother->left == nullptr || !rb_tree_is_red(brother->left)) &&
+                        (brother->right == nullptr || !rb_tree_is_red(brother->right))) { // case 2
+                        rb_tree_set_red(brother);
+                        x = xp;
+                        xp = xp->parent;
+                    } else {
+                        if (brother->left == nullptr || !rb_tree_is_red(brother->left)) { // case 3
+                            if (brother->right != nullptr)
+                                rb_tree_set_black(brother->right);
+                            rb_tree_set_red(brother);
+                            rb_tree_rotate_left(brother, root);
+                            brother = xp->left;
+                        }
+                        // 转为 case 4
+                        brother->color = xp->color;
+                        rb_tree_set_black(xp);
+                        if (brother->left != nullptr)
+                            rb_tree_set_black(brother->left);
+                        rb_tree_rotate_right(xp, root);
+                        break;
+                    }
+                }
+            }
+            if (x != nullptr) {
+                rb_tree_set_black(x);
+            }
+        }
+        return y;
+    }
+
+// 模板类 rb_tree
+// 参数一代表数据类型，参数二代表键值比较类型
+    template<class T, class Compare>
+    class rb_tree {
+    public:
+        // rb_tree 的嵌套型别定义
+        typedef rb_tree_traits<T> tree_traits;
+        typedef rb_tree_value_traits<T> value_traits;
+
+        typedef typename tree_traits::base_type base_type;
+        typedef typename tree_traits::base_ptr base_ptr;
+        typedef typename tree_traits::node_type node_type;
+        typedef typename tree_traits::node_ptr node_ptr;
+        typedef typename tree_traits::key_type key_type;
+        typedef typename tree_traits::mapped_type mapped_type;
+        typedef typename tree_traits::value_type value_type;
+        typedef Compare key_compare;
+
+        typedef mystl::allocator<T> allocator_type;
+        typedef mystl::allocator<T> data_allocator;
+        typedef mystl::allocator<base_type> base_allocator;
+        typedef mystl::allocator<node_type> node_allocator;
+
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::size_type size_type;
+        typedef typename allocator_type::difference_type difference_type;
+
+        typedef rb_tree_iterator<T> iterator;
+        typedef rb_tree_const_iterator<T> const_iterator;
+        typedef mystl::reverse_iterator<iterator> reverse_iterator;
+        typedef mystl::reverse_iterator<const_iterator> const_reverse_iterator;
+
+        allocator_type get_allocator() const { return node_allocator(); }
+
+        key_compare key_comp() const { return key_comp_; }
+
+    private:
+        // 用一下三个数据表现 rb_tree
+        base_ptr header_;  // 特殊节点，与根节点互为对方的父节点
+        size_type node_count_;  // 节点数
+        key_compare key_comp_;  // 节点键值比较的准则
+
+    private:
+        // 以下三个函数用于取得根节点，最小节点和最大节点（为什么能取到最大最小节点？）
+        base_ptr &root() const { return header_->parent; }
+
+        base_ptr &leftmost() const { return header_->left; }
+
+        base_ptr &rightmost() const { return header_->right; }
+
+    public:
+        // 构造、复制、析构函数
+        rb_tree() { rb_tree_init(); }
+
+        rb_tree(const rb_tree &rhs);
+
+        rb_tree(rb_tree &&rhs) noexcept;
+
+        rb_tree &operator=(const rb_tree &rhs);
+
+        rb_tree &operator=(rb_tree &&rhs);
+
+        ~rb_tree() { clear(); }
+
+    public:
+        // 迭代器相关操作
+        // begin 返回最小元素
+        iterator begin() noexcept { return leftmost(); }
+
+        const_iterator begin() const noexcept { return leftmost(); }
+
+        // end 返回header_
+        iterator end() noexcept { return header_; }
+
+        const_iterator end() const noexcept { return header_; }
+
+        reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+
+        const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+
+        reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+
+        const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+
+        const_iterator cbegin() const noexcept { return begin(); }
+
+        const_iterator cend() const noexcept { return end(); }
+
+        const_reverse_iterator crbegin() const noexcept { return rbegin(); }
+
+        const_reverse_iterator crend() const noexcept { return rend(); }
+    };
 
 
 }
